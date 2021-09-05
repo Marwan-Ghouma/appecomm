df493c8ba66b7787d09419723e78f27c0595839c
mr1gh <mr1gh@DESKTOP-PDC6HIJ>
2021-09-05 03:13:55 +0100

    main

--- E-commerce-Complete-Flutter-UI-master/lib/helper/ttp.dart
+++ E-commerce-Complete-Flutter-UI-master/lib/helper/ttp.dart
@@ -3,8 +3,20 @@ import 'package:shop_app/models/Product.dart';
 double ttp = 0;
 var pname = '';
 var ppt = '';
+var txt;
+double tst;
 void addproduct(Product product) {
+  tst = 1;
   ppt = product.price;
   ttp += double.parse('$ppt ');
   pname = product.title;
+  txt = '$pname ajouter avec succés';
+}
+
+void dellproduct() {
+  if (tst == 1) {
+    ttp -= double.parse('$ppt ');
+    tst = 0;
+    txt = '$pname a été supprimé ';
+  }
 }
--- E-commerce-Complete-Flutter-UI-master/lib/screens/cart/cart_screen.dart
+++ E-commerce-Complete-Flutter-UI-master/lib/screens/cart/cart_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app/models/Cart.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:shop_app/screens/home/home_screen.dart';
 
 import 'components/body.dart';
 import 'components/check_out_card.dart';
+import 'package:shop_app/helper/ttp.dart';
 
 class CartScreen extends StatelessWidget {
   static String routeName = "/cart";
@@ -10,7 +13,29 @@ class CartScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(context),
-      body: Body(),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(10, 150, 10, 0),
+          child: Column(
+            children: [
+              Text(
+                '$txt',
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.pinkAccent,
+                ),
+              ),
+              IconButton(
+                icon: SvgPicture.asset("assets/icons/Trash.svg"),
+                onPressed: () {
+                  dellproduct();
+                  Navigator.pushNamed(context, HomeScreen.routeName);
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
       bottomNavigationBar: CheckoutCard(),
     );
   }
